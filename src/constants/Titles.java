7e9f27883434c6a77e7613895c5f276e448bdaa2
LumiaJ <33870129+LumiaJ@users.noreply.github.com>
2020-06-30 15:59:29 +0800

    Add files via upload

---
+++ src/constants/Titles.java
@@ -0,0 +1,9 @@
+package constants;
+
+public class Titles {
+	public static final String[] YEARS = {"2010年", "2011年", "2012年",
+			"2013年", "2014年", "2015年", "2016年", "2017年", "2018年", };
+	public static final String[] MONTHS = {"1月", "2月", "3月", "4月", 
+			"5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月", "全年"};
+	public static final String[] CHANGE = {"减少了","增加了"};
+}
